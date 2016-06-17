ed99c43c06936e8e412e4fbc20638af1060b7dca
VladimirChocceAparco <vladicha0912@gmail.com>
2016-06-17 17:58:57 +0200

    operaciones terminadas...

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -23,4 +23,10 @@ public class Calculadora {
     float resta(float a,float b){
         return a-b;
     }
+    float multiplicacion(float a,float b){
+        return a*b;
+    }
+    float division(float a,float b){
+        return a/b;
+    }
 }
